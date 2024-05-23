e73e71eceeadbd215f3e065a14d4b659945152fb
BadAimWeeb <badaimweeb@protonmail.com>
2024-05-23 12:54:39 -0500

    uhhhhhh

--- index.ts
+++ index.ts
@@ -56,12 +56,12 @@ const httpServer = Bun.serve({
             return rt;
         }
 
-        if (requestURL.pathname === "/openapi.yaml") {
-            let oapi = fs.readFileSync("./openapi.yaml", "utf-8");
-            return new Response(oapi.replace("- url: https", `- url: ${requestURL.protocol}//${requestURL.hostname}${requestURL.port !== (requestURL.protocol === "https:" ? "443" : "80") ? ":" + requestURL.port : ""}`), {
-                headers: { 'content-type': 'application/yaml', 'access-control-allow-origin': '*' }
-            });
-        }
+        // if (requestURL.pathname === "/openapi.yaml") {
+        //     let oapi = fs.readFileSync("./openapi.yaml", "utf-8");
+        //     return new Response(oapi.replace("- url: https", `- url: ${requestURL.protocol}//${requestURL.hostname}${requestURL.port !== (requestURL.protocol === "https:" ? "443" : "80") ? (":" + requestURL.port) : ""}`), {
+        //         headers: { 'content-type': 'application/yaml', 'access-control-allow-origin': '*' }
+        //     });
+        // }
 
         // this code kinda sucks
         if (requestURL.pathname.startsWith("/docs/") || requestURL.pathname === "/docs") {
@@ -77,7 +77,8 @@ const httpServer = Bun.serve({
                 return new Response(`
                     window.onload = function() {
                         window.ui = SwaggerUIBundle({
-                            url: "${requestURL.protocol}//${requestURL.hostname}${requestURL.port !== (requestURL.protocol === "https:" ? "443" : "80") ? ":" + requestURL.port : ""}/openapi.yaml",
+                            //url: "${requestURL.protocol}//${requestURL.hostname}${requestURL.port !== (requestURL.protocol === "https:" ? "443" : "80") ? (":" + requestURL.port) : ""}/openapi.yaml",
+                            url: "/docs/openapi.yaml",
                             dom_id: '#swagger-ui',
                             deepLinking: true,
                             presets: [
